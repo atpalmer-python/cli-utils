298f4fe52f1a093516c159d7a0da07ed21d0a05d
Andrew Palmer <atp@sdf.org>
2022-01-24 14:04:50 -0600

    Update unixtime setup.py

--- unixtime-cli/setup.py
+++ unixtime-cli/setup.py
@@ -5,13 +5,12 @@ from time import time
 setup(
     name='unixtime',
     version=int(time()),
-    author='Andy Palmer',
-    author_email='andrew.t.palmer@parker.com',
     packages=find_packages(),
     entry_points={
         'console_scripts': ['unixtime=unixtime.main:main'],
     },
     install_requires=[
-        'argparse'
+        'argparse',
+        'python-dateutil',
     ]
 )
